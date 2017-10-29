d7e97e6bcade0ae2be9c179bb70e47f969036f75
Arafat Hasan Jenin <md.arafathasanjenin@gmail.com>
2017-10-29 19:08:07 +0600

    README.md updated, unnecessary header files excluded from uva-tool.cpp

--- uva-tool.cpp
+++ uva-tool.cpp
@@ -28,77 +28,13 @@
 
 ///////////////////////////////////////////////////////////////////////
 
-#include <iostream>
-#include <climits>
-#include <cmath>
-#include <cstring>
-#include <cctype>
-#include <cstdio>
-#include <cstdlib>
-#include <iomanip>
-#include <utility>
-#include <sstream>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <list>
-#include <map>
-#include <unordered_map>
-#include <queue>
-#include <deque>
-#include <vector>
-#include <stdint.h> //uint32_t
-#include <functional>
-#include <bitset>
-
-#include <cstdio>
-#include <fstream>
-#include <iostream>
-#include <memory>
-#include <stdexcept>
-#include <string>
-#include <array>
-#include "include/json.hpp"
+#include <iostream> // std IO
+#include <fstream> // std::ifstream
+#include "include/json.hpp" // json praser
 #include <unistd.h> // getlogin()
 
 using namespace std;
 using json = nlohmann::json;
-
-typedef long long           ll;
-typedef double              lf;
-typedef unsigned long long  ull;
-typedef pair<int, int>      pii;
-typedef vector<pii>         vpii;
-typedef vector<int>         vi;
-
-#define __FastIO        ios_base::sync_with_stdio(false); cin.tie(0)
-
-#define forr(i, a, b)   for (__typeof (a) i=a; i<=b; i++)
-#define rof(i, b, a)    for (__typeof (a) i=b; i>=a; i--)
-#define rep(i, n)       for (__typeof (n) i=0; i<n; i++)
-#define forit(i, s)     for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
-#define all(ar)         ar.begin(), ar.end()
-#define fill(ar, val)   memset(ar, val, sizeof(ar))
-#define clr(a)          memset(a, 0, sizeof(a))
-
-#define nl              cout << '\n';
-#define sp              cout << ' ';
-#define gc              getchar
-#define chk             cout << "##########\n"
-#define pb              push_back
-#define debug1(x)       cout << #x << ": " << x << endl
-#define debug2(x, y)    cout << #x << ": " << x << '\t' << #y << ": " << y << endl
-#define debug3(x, y, z) cout << #x << ": " << x << '\t' << #y << ": " << y << '\t' << #z << ": " << z << endl
-
-#define max(a, b)       (a < b ? b : a)
-#define min(a, b)       (a > b ? b : a)
-#define sq(a)           (a * a)
-
-#define PI              acos(-1.0)
-#define INF             0x7fffffff
-#define MOD             1000000007
-#define EPS             1e-7
-#define MAX             10000005
 
 #ifdef __linux__
 const string szHome = getlogin();
@@ -273,8 +209,8 @@ public:
         cout << left << setw(11) << "Sub ID" << setw(40) << "Problem\t" << \
              "Verdict\t\t" << "Runtime\t" << "Rank\t" << "Language   " << \
              "Date\n";
-        cout << "=========================================================\
-=================================================================\n";
+        cout << "=============================================================";
+        cout << "=============================================================\n";
         for (int i = range - 1; i >= 0; i--) {
             sid(json_subs["subs"][i][0]);
             problem_name(json_subs["subs"][i][1]);
