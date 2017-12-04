1e4c786b1e23ef4fb632eca12fca1467945d23a2
Arafat Hasan Jenin <md.arafathasanjenin@gmail.com>
2017-12-05 01:31:12 +0600

    getlogin

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
+sed -i "38s/.*/\	const\ string\ szHome\ =\ ${1};/" src/uva-tool.cpp
 g++ -g -Wall -Wextra -Wshadow -Wfloat-equal -pedantic -std=c++11 -O2 -Wformat=2 -Wconversion -lm -o uva-tool src/uva-tool.cpp
+mkdir ~/.cache/uva-tool/
 sudo mv uva-tool /usr/bin/
 sudo mkdir /usr/share/uva-tool/
 sudo cp pid-to-num.cvs /usr/share/uva-tool/
-mkdir ~/.cache/uva-tool/
+
 
