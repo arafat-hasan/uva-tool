e90ac4ed004168a313d6051c0b8a5bc2f19dc669
Arafat Hasan Jenin <md.arafathasanjenin@gmail.com>
2018-10-16 21:30:33 +0600

    test travis

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo ${1}
 echo
 sed -i "38s/.*/\	const\ string\ szHome\ =\ ${1};/" src/uva-tool.cpp
-g++-7 -g -Wall -Wextra -Wshadow -Wfloat-equal -pedantic -std=c++11 -O2 -Wformat=2 -Wconversion -lm -o uva-tool src/uva-tool.cpp
+g++ -g -Wall -Wextra -Wshadow -Wfloat-equal -pedantic -std=c++11 -O2 -Wformat=2 -Wconversion -lm -o uva-tool src/uva-tool.cpp
 mkdir ~/.cache/uva-tool/
 sudo mv uva-tool /usr/bin/
 sudo mkdir /usr/share/uva-tool/
